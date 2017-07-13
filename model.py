5b4bc0754e3c240bd18dafa25f36cc2e1fee4e8e
Andrew Garkavyi <andrew.gawy@gmail.com>
2017-07-13 01:02:17 -0700

    Generator, randomization, run params

--- model.py
+++ model.py
@@ -5,11 +5,14 @@ from keras.models import Sequential
 from keras.layers import Activation, Dense, Flatten, Convolution2D, MaxPool2D, Cropping2D, Lambda, Input, Dropout
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
+from keras.utils import plot_model
 
 import csv
 import numpy as np
 import random as rnd
 import cv2
+import math
+import time
 
 from sklearn.utils import shuffle as sk_shuffle
 from sklearn.model_selection import train_test_split
@@ -87,14 +90,15 @@ def create_vgg():
 
 
 # Executes training code for any Keras model.
-def train_in_driving_with_generator(train_gen, valid_gen, train_data_len, valid_data_len, model):
+def train_in_driving_with_generator(train_gen, valid_gen, train_data_len, valid_data_len, model, epochs_count=3):
     log = logging.getLogger(__name__)
 
     log.info('Start trainig...')
 
     #Compile and train model
     model.compile('adam', 'mean_squared_error')
-    model.fit_generator(train_gen, train_data_len, epochs=3, validation_data=valid_gen, validation_steps=valid_data_len)
+    model.fit_generator(train_gen, train_data_len, epochs=epochs_count, validation_data=valid_gen,
+                        validation_steps=valid_data_len)
 
     log.info('Saving model to model.h5')
     model.save("model.h5")
@@ -132,9 +136,10 @@ def read_all_data_to_memory():
     log.info('All data read to memory')
     return x_data, y_data
 
+
 # Read CSV file and create train/validation geneators that will be used while training.
 # returns: tuple of (train_generator, valid_generator) - generators to load train and validation data when training
-def create_data_generators():
+def create_data_generators(batch_size=32):
     log = logging.getLogger(__name__)
 
     log.info('Reading csv file...')
@@ -146,15 +151,20 @@ def create_data_generators():
         next(csv_reader) #just skip the header line for test data set provided by Udacity
 
         for row in csv_reader:
+            s_angle = float(row[3])
+            #if abs(s_angle) < 0.05 and rnd.random() <= 0.55: continue
             samples.append(row)
 
     train_samples, valid_samples = train_test_split(samples, test_size=0.2)
-    log.info('Samples read. Sending data to generator...')
+    log.info('Samples read, {} lines. Sending data to generator...'.format(len(samples)))
 
-    generator_for_train = input_generator(train_samples, 32)
-    generator_for_valid = input_generator(valid_samples, 32)
+    generator_for_train = input_generator(train_samples, batch_size)
+    generator_for_valid = input_generator(valid_samples, batch_size)
 
-    return generator_for_train, generator_for_valid, len(train_samples), len(valid_samples)
+    return generator_for_train, generator_for_valid, \
+           math.ceil(len(train_samples) / batch_size), math.ceil(len(valid_samples) / batch_size)
+    #3 images per each sample line
+
 
 # Generator to load data for model training
 def input_generator(data, batch_size=32):
@@ -164,11 +174,13 @@ def input_generator(data, batch_size=32):
         for offset in range(0, data_length, batch_size):
             samples = data[offset:offset + batch_size]
             log.debug('Batch offset:{}, len:{}'.format(offset, len(samples)))
+            t = time.time()
 
             x_data, y_data = load_samples(samples)
 
-            log.debug('Data batch ready')
+            log.debug('Data batch ready in {}'.format(time.time() - t))
             yield x_data, y_data
+
 
 # Convenience method used to load batch of data either by generator or as a whole into memory.
 # samples - rows from csv file containing training information.
@@ -179,11 +191,10 @@ def load_samples(samples):
     y_data = []
     for row in samples:
         s_angle = float(row[3])
-        if abs(s_angle) < 0.05 and rnd.random() <= 0.85: continue
         im_path, l_im_path, r_im_path = row[0], row[1], row[2]
 
         load_image(im_path, s_angle, x_data, y_data)
-        correction_angle = 0.21
+        correction_angle = 0.25
         load_image(l_im_path, s_angle + correction_angle, x_data, y_data)
         load_image(r_im_path, s_angle - correction_angle, x_data, y_data)
     log.debug('Processing image files: converting X...')
@@ -193,8 +204,8 @@ def load_samples(samples):
     log.debug('Processing image files: Y...')
     y_data = np.array(y_data)
     x_data, y_data = sk_shuffle(x_data, y_data)
-    log.debug(x_data[10])
-    log.debug(y_data[:20])
+    # log.debug(x_data[10])
+    # log.debug(y_data[:20])
     return x_data, y_data
 
 
@@ -208,35 +219,46 @@ def load_image(im_path, s_angle, x_data, y_data):
     path = im_path[im_path.rfind('/'):]
     im = cv2.imread('./data/IMG' + path)
     # im = cv2.resize(im, (224,224,3), interpolation=cv2.INTER_AREA)
-    # process image
-    x_data.append(im)
-    y_data.append(s_angle)
 
-    # add flipped images to avoid one side bias
-    y_data.append(-(s_angle))
-    x_data.append(np.fliplr(im))
+    rand = rnd.random()
+
+    if rand <= 0.5:
+        # process image
+        x_data.append(im)
+        y_data.append(s_angle)
+
+    if rand > 0.5:
+        # add flipped images to avoid one side bias
+        y_data.append(-(s_angle))
+        x_data.append(np.fliplr(im))
 
 
 # Execute when ran from terminal
 
 if __name__ == '__main__' :
-    logging.basicConfig(level=logging.INFO)
-    log = logging.getLogger(__name__)
-
     parser = argparse.ArgumentParser()
     parser.add_argument('epochs', type=int, default=3, help='Amount of epochs to run training for')
+    parser.add_argument('-d', action='store_true', help='Debug mode')
 
     args = parser.parse_args()
     epochs_count = args.epochs
 
+    if args.d:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger(__name__)
+
     # with generators
-    # train_gen, valid_gen, train_len, valid_len = create_data_generators()
-    #
-    # model = drive_model_create()# create_vgg()
-    # train_in_driving_with_generator(train_gen, valid_gen, train_len, valid_len, model)
+    train_gen, valid_gen, train_steps, valid_steps = create_data_generators(128)
+    log.info('Train set len={}, valid_len={}'.format(train_steps, valid_steps))
+
+    model = drive_model_create()# create_vgg()
+    train_in_driving_with_generator(train_gen, valid_gen, train_steps, valid_steps, model, epochs_count)
+    plot_model(model, to_file='model.png')
 
 
     # without generator
-    model = drive_model_create()
-    x_data, y_data = read_all_data_to_memory()
-    train_in_driving(x_data, y_data, model, epochs_count)
+    # model = drive_model_create()
+    # x_data, y_data = read_all_data_to_memory()
+    # train_in_driving(x_data, y_data, model, epochs_count)
